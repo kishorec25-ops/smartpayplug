2f9fbe500980f27b0b14ced9e9d343892670b543
kishorec25-ops <kishor.ec25@stellamaryscoe.edu.in>
2026-03-08 13:49:32 +0530

    Set up Express server with payment and status routes

--- server.js
+++ server.js
@@ -1,1 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+let powerStatus = false;
+
+app.get("/", (req,res)=>{
+  res.send("SmartPay Plug API running");
+});
+
+app.get("/pay",(req,res)=>{
+  powerStatus = true;
+  res.json({message:"Payment successful. Power enabled"});
+});
+
+app.get("/status",(req,res)=>{
+  res.json({power:powerStatus});
+});
+
+app.listen(PORT,()=>{
+  console.log("Server running");
+});
 
